d6bfd0c352a75a76a7c516366dcd46b8560a5976
Jaydeep Rayat <jaydeep080805@gmail.com>
2024-01-24 13:21:22 +0000

    removed japjibuttons and moved code to translationButton

--- src/translations/components/translationButton.jsx
+++ src/translations/components/translationButton.jsx
@@ -1,5 +1,26 @@
-function TranslationButton() {
-  return <></>;
+import Button from "../../components/Button";
+import PropTypes from "prop-types";
+
+function TranslationButton({ onLanguageChange }) {
+  // when one of the buttons is clicked, set the language to what the button is named
+  function handleClick(buttonName) {
+    onLanguageChange(buttonName);
+  }
+
+  return (
+    <>
+      <Button onClickFunc={() => handleClick("punjabi")} name={"punjabi"} />
+      <Button onClickFunc={() => handleClick("english")} name={"english"} />
+      <Button
+        onClickFunc={() => handleClick("english meaning")}
+        name={"english meaning"}
+      />
+    </>
+  );
 }
+
+TranslationButton.propTypes = {
+  onLanguageChange: PropTypes.func.isRequired,
+};
 
 export default TranslationButton;
