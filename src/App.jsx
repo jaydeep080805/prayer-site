5b2b496d624813fd68c102f478ebf27f0e71d7bb
Jaydeep Rayat <jaydeep080805@gmail.com>
2024-01-29 14:46:26 +0000

    added ardaas and sohilla routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import Home from "./Home";
 import TranslationSelectionPage from "./translationPages/TranslationSelectionPage";
 import JapjiSahib from "./translationPages/JapjiSahib";
 import RaehraasSahib from "./translationPages/RaehraasSahib";
+import ArdasSahib from "./translationPages/ArdasSahib";
+import SohillaSahib from "./translationPages/SohillaSahib";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <div className="h-full bg-gray-100 md:h-fit">
+      <div className="h-full bg-gray-100 overflow-x-hidden md:h-fit">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/translations" element={<TranslationSelectionPage />} />
           <Route path="/japji-sahib" element={<JapjiSahib />} />
           <Route path="/raehraas-sahib" element={<RaehraasSahib />} />
+          <Route path="/ardaas-sahib" element={<ArdasSahib />} />
+          <Route path="/sohilaa-sahib" element={<SohillaSahib />} />
         </Routes>
       </div>
     </BrowserRouter>
