8ca8473a7283a3048d2c8354557be32643c2344b
Jaydeep Rayat <jaydeep080805@gmail.com>
2024-01-29 14:47:18 +0000

    removed w-screen which was causing weird overflow

--- src/navbar/desktopNavbar/DesktopNavbar.jsx
+++ src/navbar/desktopNavbar/DesktopNavbar.jsx
@@ -5,7 +5,7 @@ import Border from "../components/Border";
 
 function DesktopNavbar() {
   return (
-    <div className="hidden bg-white w-screen md:flex flex-col">
+    <div className="hidden bg-white md:flex flex-col">
       <div className="flex justify-center py-4 space-x-80">
         {/* left side */}
         <Image image={Khanda} alt={"Sikh Khanda"} className="bg-transparent	" />
