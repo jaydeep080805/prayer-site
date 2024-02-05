47847e973745945538e6d348205eec8732a07981
Jaydeep Rayat <jaydeep080805@gmail.com>
2024-02-05 14:30:49 +0000

    added hover animation to image

--- src/sikhGurus/components/GuruImage.jsx
+++ src/sikhGurus/components/GuruImage.jsx
@@ -6,7 +6,7 @@ function GuruImage({ imageSrc }) {
       <img
         src={imageSrc}
         // create a border and set the sizing for different device sizes
-        className="border border-gray-300 rounded-md w-60 h-60 lg:w-80 lg:h-80 xl:w-96 xl:h-96"
+        className="border border-gray-300 rounded-md w-60 h-60 lg:w-80 lg:h-80 lg:hover:scale-105 lg:transition lg:duration-200 xl:w-96 xl:h-96"
       />
     </div>
   );
