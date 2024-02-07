89890fe1b1eafb926082b7f9217e523e4a26f4fb
Jaydeep Rayat <jaydeep080805@gmail.com>
2024-02-07 21:25:53 +0000

    changed data

--- src/calendar/EventTable.jsx
+++ src/calendar/EventTable.jsx
@@ -17,26 +17,50 @@ function EventTable() {
       <tbody>
         <tr>
           <td className="px-2 py-2 border-b font-main lg:px-8">
-            Guru Gobind Singh Ji&apos;s Birthday
+            Maghi - Lohri
           </td>
-          <td className="px-2 py-2 border-b font-main lg:px-8">January 5</td>
           <td className="px-2 py-2 border-b font-main lg:px-8">
-            Celebrates the birth of the 10th Sikh Guru.
+            Jan 13, 2024 (Saturday)
           </td>
-        </tr>
-        <tr>
-          <td className="px-2 py-2 border-b font-main lg:px-8">
-            Maghi - Mela Maghi
-          </td>
-          <td className="px-2 py-2 border-b font-main lg:px-8">January 14</td>
           <td className="px-2 py-2 border-b font-main lg:px-8">
             Commemorates the martyrdom of the Forty Immortals who fought
             valiantly for Guru Gobind Singh Ji.
           </td>
         </tr>
         <tr>
+          <td className="px-2 py-2 border-b font-main lg:px-8">
+            Guru Gobind Singh Birthday
+          </td>
+          <td className="px-2 py-2 border-b font-main lg:px-8">
+            Jan 17, 2024 (Wednesday)
+          </td>
+          <td className="px-2 py-2 border-b font-main lg:px-8">
+            Celebrates the birth of the 10th Sikh Guru.
+          </td>
+        </tr>
+        <tr>
+          <td className="px-2 py-2 border-b font-main lg:px-8">Holi</td>
+          <td className="px-2 py-2 border-b font-main lg:px-8">
+            Mar 25, 2024 (Monday)
+          </td>
+          <td className="px-2 py-2 border-b font-main lg:px-8">
+            Festival of colors.
+          </td>
+        </tr>
+        <tr>
+          <td className="px-2 py-2 border-b font-main lg:px-8">Hola Mohalla</td>
+          <td className="px-2 py-2 border-b font-main lg:px-8">
+            Mar 27, 2024 (Wednesday)
+          </td>
+          <td className="px-2 py-2 border-b font-main lg:px-8">
+            Sikh festival.
+          </td>
+        </tr>
+        <tr>
           <td className="px-2 py-2 border-b font-main lg:px-8">Vaisakhi</td>
-          <td className="px-2 py-2 border-b font-main lg:px-8">April 14</td>
+          <td className="px-2 py-2 border-b font-main lg:px-8">
+            Apr 13, 2024 (Saturday)
+          </td>
           <td className="px-2 py-2 border-b font-main lg:px-8">
             Marks the Sikh New Year and the formation of the Khalsa in 1699 by
             Guru Gobind Singh Ji.
@@ -44,9 +68,22 @@ function EventTable() {
         </tr>
         <tr>
           <td className="px-2 py-2 border-b font-main lg:px-8">
-            Martyrdom of Guru Arjan Dev Ji
+            Birthday of Guru Angad Dev
           </td>
-          <td className="px-2 py-2 border-b font-main lg:px-8">June 16</td>
+          <td className="px-2 py-2 border-b font-main lg:px-8">
+            Apr 18, 2024 (Thursday)
+          </td>
+          <td className="px-2 py-2 border-b font-main lg:px-8">
+            Celebrates the birth of Guru Angad Dev.
+          </td>
+        </tr>
+        <tr>
+          <td className="px-2 py-2 border-b font-main lg:px-8">
+            Martyrdom of Guru Arjan Dev Sahib
+          </td>
+          <td className="px-2 py-2 border-b font-main lg:px-8">
+            Jun 16, 2024 (Sunday)
+          </td>
           <td className="px-2 py-2 border-b font-main lg:px-8">
             Honors the fifth Sikh Guru, who was martyred in 1606 by the Mughal
             emperor for refusing to convert to Islam.
@@ -54,28 +91,43 @@ function EventTable() {
         </tr>
         <tr>
           <td className="px-2 py-2 border-b font-main lg:px-8">
-            First Prakash Utsav of Guru Granth Sahib Ji
+            Birth of the Guru Granth
           </td>
-          <td className="px-2 py-2 border-b font-main lg:px-8">September 1</td>
+          <td className="px-2 py-2 border-b font-main lg:px-8">
+            Oct 20, 2024 (Sunday)
+          </td>
           <td className="px-2 py-2 border-b font-main lg:px-8">
             Celebrates the first installation of the Guru Granth Sahib in the
             Golden Temple in 1604.
           </td>
         </tr>
         <tr>
+          <td className="px-2 py-2 border-b font-main lg:px-8">Diwali</td>
           <td className="px-2 py-2 border-b font-main lg:px-8">
-            Guru Nanak Dev Ji&apos;s Birthday
+            Nov 01, 2024 (Friday)
           </td>
-          <td className="px-2 py-2 border-b font-main lg:px-8">November 8</td>
+          <td className="px-2 py-2 border-b font-main lg:px-8">
+            Festival of lights.
+          </td>
+        </tr>
+        <tr>
+          <td className="px-2 py-2 border-b font-main lg:px-8">
+            Guru Nanak Birthday
+          </td>
+          <td className="px-2 py-2 border-b font-main lg:px-8">
+            Nov 15, 2024 (Friday)
+          </td>
           <td className="px-2 py-2 border-b font-main lg:px-8">
             Celebrates the birth of Guru Nanak, the founder of Sikhism, in 1469.
           </td>
         </tr>
         <tr>
           <td className="px-2 py-2 border-b font-main lg:px-8">
-            Martyrdom of Guru Tegh Bahadur Ji
+            Martyrdom of Guru Tegh Bahadur Sahib
           </td>
-          <td className="px-2 py-2 border-b font-main lg:px-8">November 24</td>
+          <td className="px-2 py-2 border-b font-main lg:px-8">
+            Nov 24, 2024 (Sunday)
+          </td>
           <td className="px-2 py-2 border-b font-main lg:px-8">
             Commemorates the sacrifice of the ninth Sikh Guru, who was executed
             in Delhi by Aurangzeb for refusing to convert to Islam.
