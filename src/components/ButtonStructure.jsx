969a565a4bb5504d09695bc0e223454864a28403
Jaydeep Rayat <jaydeep080805@gmail.com>
2024-02-07 13:35:45 +0000

    changed md to lg breakpoint

--- src/components/ButtonStructure.jsx
+++ src/components/ButtonStructure.jsx
@@ -7,7 +7,7 @@ function ButtonStructure({ buttons }) {
     // center the elements
     <div className="flex justify-center">
       {/* set a max width so the elements can wrap*/}
-      <div className="md:max-w-[80rem]">
+      <div className="lg:max-w-[80rem]">
         {/* the container holding all the buttons */}
         {/* sets the display to a flex-col on mobile and flew-row with flex-wrap on bigger screens */}
         <div className="mt-5 font-main flex flex-col items-center gap-5 md:flex-row md:flex-wrap md:justify-center md:text-center">
