4cc1aba01082a278f249270939d64716be7059a4
Jaydeep Rayat <jaydeep080805@gmail.com>
2024-02-10 00:20:43 +0000

    added alt text

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
 
-function Image({ imageSrc, isEvent = false }) {
+function Image({ imageSrc, altText, isEvent = false }) {
   return (
     <div className="flex justify-center mt-5">
       <img
         src={imageSrc}
+        alt={altText}
         // create a border and set the sizing for different device sizes
         className={`border border-gray-300 rounded-md w-60 h-60 lg:w-80 lg:h-80 lg:hover:scale-105 lg:transition lg:duration-200 xl:w-96 xl:h-96
         ${isEvent && "w-80 h-40 lg:w-96 lg:h-60 xl:w-[25rem] xl:h-60"}`}
@@ -15,6 +16,7 @@ function Image({ imageSrc, isEvent = false }) {
 
 Image.propTypes = {
   imageSrc: PropTypes.string.isRequired,
+  altText: PropTypes.string,
   isEvent: PropTypes.bool,
 };
 
