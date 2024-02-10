ea34f6f3b5b529f936606849ddc6e41213029800
Jaydeep Rayat <jaydeep080805@gmail.com>
2024-02-10 19:41:22 +0000

    mobile navbar now should stop the scroll of the page and allow scrolling in the sidepanel

--- src/navbar/mobileNavbar/components/SlideMenu.jsx
+++ src/navbar/mobileNavbar/components/SlideMenu.jsx
@@ -1,12 +1,30 @@
 import PropTypes from "prop-types";
 import MobileMenu from "./MobileMenu";
+import { useEffect } from "react";
 
 // the container for the navbar menu on mobile
 function SlideMenu({ isOpened, clickFunc }) {
+  // Use useEffect to toggle overflow on the body
+  useEffect(() => {
+    if (isOpened) {
+      // stop the page from being scrollable
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+
+    // Clean up function
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+
+    // run the effect everytime isOpened changes
+  }, [isOpened]);
+
   return (
     // pass through if the menu is opened to show or hide it
     <div
-      className={`bg-white dark:bg-gray-800 h-full w-3/5 right-0 z-20 fixed transform transition-transform duration-300 ${
+      className={`bg-white overflow-y-scroll dark:bg-gray-800 h-full w-3/5 right-0 z-20 fixed transform transition-transform duration-300 ${
         isOpened ? "translate-x-0" : "translate-x-full"
       }`}
     >
