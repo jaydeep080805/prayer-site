aa36d443294ea0a3b7129d8e19749ad1fc5589e8
Jaydeep Rayat <jaydeep080805@gmail.com>
2024-02-12 14:34:55 +0000

    removed hover effects from mobile

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,7 +9,7 @@ function Button({ name, pageLink, onClickFunc = null }) {
     <Link
       to={pageLink}
       onClick={onClickFunc}
-      className="bg-white dark:bg-gray-800 capitalize font-bold text-orange-500 w-80 mx-5 p-5 rounded-md shadow-lg cursor-pointer hover md:mx-0 md:hover:scale-105 md:hover:bg-gray-200 dark:hover:bg-gray-700 transition duration-200"
+      className="bg-white dark:bg-gray-800 capitalize font-bold text-orange-500 w-80 mx-5 p-5 rounded-md shadow-lg cursor-pointer lg:mx-0 lg:hover:scale-105 lg:hover:bg-gray-200 dark:lg:hover:bg-gray-700 transition duration-200"
     >
       <p className="text-4xl md:text-3xl">{name}</p>
     </Link>
