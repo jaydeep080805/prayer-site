a619617ce29e3c2c98e0b3b2f17916ffce4f28d2
Jaydeep Rayat <jaydeep080805@gmail.com>
2024-02-12 17:17:56 +0000

    changed div to h1 tag

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -3,9 +3,9 @@ import PropTypes from "prop-types";
 function Heading({ text }) {
   return (
     // the main heading at the top of the page
-    <div className="flex flex-col font-bold font-heading text-4xl pt-5 text-orange-400 underline capitalize">
+    <h1 className="flex flex-col font-bold font-heading text-4xl pt-5 text-orange-400 underline capitalize">
       {text}
-    </div>
+    </h1>
   );
 }
 
