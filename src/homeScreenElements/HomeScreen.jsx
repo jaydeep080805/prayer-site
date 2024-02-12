cd5ec32244a56fae2d92598945baf400b1d5dbc4
Jaydeep Rayat <jaydeep080805@gmail.com>
2024-02-12 17:18:19 +0000

    added heading text and fixed spacing

--- src/homeScreenElements/HomeScreen.jsx
+++ src/homeScreenElements/HomeScreen.jsx
@@ -1,41 +1,43 @@
 import HomeScreenButtons from "./HomeScreenButtons";
 import ButtonStructure from "../components/ButtonStructure";
 import Card from "../components/Card";
+import Heading from "../components/Heading";
 
 function HomeScreen() {
   // passes through the buttons into the button structure
   return (
-    <div className="flex flex-col items-center justify-evenly">
+    <div className="flex flex-col items-center text-center justify-evenly gap-5 mt-3">
+      <Heading text="welcome to sikh essence" />
+
       <ButtonStructure buttons={<HomeScreenButtons />} />
 
       <br />
-      <br />
-      <br />
-      <br />
 
-      <h2 className="font-heading text-4xl mb-4 mx-10 text-center font-semibold">
-        Not Sure Where To Start?
-      </h2>
-      <div className="border border-b border-orange-400 w-40 mb-14 lg:w-80"></div>
+      <div className="mt-2 flex flex-col items-center">
+        <h2 className="font-heading text-4xl mb-4 mx-10 text-center font-semibold">
+          Not Sure Where To Start?
+        </h2>
+        <div className="border border-b border-orange-400 w-40 self-center mb-8 lg:w-80"></div>
 
-      <div className="flex flex-col w-80 gap-10 lg:flex-row lg:w-full lg:justify-center">
-        <Card
-          pageLink="/fivek"
-          title="five k's"
-          text="Learn the 5 items that devoted sikhs should be wearing at all times"
-        />
+        <div className="flex flex-col w-80 gap-10 lg:flex-row lg:w-full lg:justify-center">
+          <Card
+            pageLink="/fivek"
+            title="five k's"
+            text="Learn the 5 items that devoted sikhs should be wearing at all times"
+          />
 
-        <Card
-          pageLink="/calendar"
-          title="calendar"
-          text="check the list of sikh festivals, events and holidays"
-        />
+          <Card
+            pageLink="/calendar"
+            title="calendar"
+            text="check the list of sikh festivals, events and holidays"
+          />
 
-        <Card
-          pageLink="/japji-sahib"
-          title="japji sahib"
-          text="have a read of the first prayer sikhs should read when waking up"
-        />
+          <Card
+            pageLink="/japji-sahib"
+            title="japji sahib"
+            text="have a read of the first prayer sikhs should read when waking up"
+          />
+        </div>
       </div>
     </div>
   );
