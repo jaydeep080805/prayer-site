7baf09952e58c4a20e8dd4b12c2fbdef322cff2d
Jaydeep Rayat <jaydeep080805@gmail.com>
2024-02-14 15:35:50 +0000

    changed arrow function to regular function

--- src/translationPages/components/PrayerContent.jsx
+++ src/translationPages/components/PrayerContent.jsx
@@ -5,15 +5,17 @@ function PrayerContent({ script, selectedLanguage, prayerName }) {
   return (
     <>
       {/* loop through the array and send each line to the prayerLine where it will check how it should be rendered */}
-      {script.map((line, index) => (
-        <PrayerLine
-          key={index}
-          line={line}
-          index={index}
-          selectedLanguage={selectedLanguage}
-          prayerName={prayerName}
-        />
-      ))}
+      {script.map(function (line, index) {
+        return (
+          <PrayerLine
+            key={index}
+            line={line}
+            index={index}
+            selectedLanguage={selectedLanguage}
+            prayerName={prayerName}
+          />
+        );
+      })}
     </>
   );
 }
