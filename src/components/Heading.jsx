ae50638118e1469f03db604fe89f280bc1e51cf3
Jaydeep Rayat <jaydeep080805@gmail.com>
2024-02-14 15:37:07 +0000

    added prayer heading

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -3,8 +3,16 @@ import PropTypes from "prop-types";
 function Heading({ text }) {
   return (
     // the main heading at the top of the page
-    <h1 className="flex flex-col font-bold font-heading text-4xl pt-5 text-orange-400 underline capitalize">
+    <h1 className="font-bold font-heading text-4xl pt-5 text-orange-400 underline capitalize">
       {text}
+    </h1>
+  );
+}
+
+function PrayerHeading({ text }) {
+  return (
+    <h1 className="capitalize text-orange-400 text-4xl font-heading font-bold underline">
+      {text} sahib
     </h1>
   );
 }
@@ -13,4 +21,9 @@ Heading.propTypes = {
   text: PropTypes.string.isRequired,
 };
 
+PrayerHeading.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
 export default Heading;
+export { PrayerHeading };
