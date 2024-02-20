23c82363fecab8587db6159bbee78fa857fc0b22
Jaydeep Rayat <jaydeep080805@gmail.com>
2024-02-20 11:22:52 +0000

    refactored code to use arrays and .map instead of using multiple components

--- src/sikhism/GranthSahib.jsx
+++ src/sikhism/GranthSahib.jsx
@@ -2,49 +2,43 @@ import ContentWithSubheading from "../components/ContentWithSubheading";
 import Section from "../components/Section";
 import PageLayout from "../components/PageLayout";
 
+const granthSahibData = [
+  {
+    text: "The Guru Granth Sahib is the central religious scripture of Sikhism, revered not just as a sacred text but as the eternal Guru and the final authority in Sikh life and spirituality. Compiled and completed in 1604 by the fifth Sikh Guru, Guru Arjan Dev Ji, it embodies the teachings, wisdom, and divine poetry of the Sikh Gurus, along with saints and scholars from various religious backgrounds, reflecting Sikhism's inclusive ethos.",
+  },
+  {
+    subheading: "Composition and Contributors",
+    text: "The Guru Granth Sahib is a voluminous text composed in the Gurmukhi script and contains 1430 Angs (pages). It includes the compositions of the Sikh Gurus, from Guru Nanak Dev Ji to Guru Tegh Bahadur, and enlightened souls like Bhagat Kabir, Sheikh Farid, Bhagat Ravidas, and many others, making it a unique scripture that transcends religious boundaries. This diversity underscores the Sikh belief in the universality of God and the unity of all humanity.",
+  },
+  {
+    subheading: "Teachings and Significance",
+    text: "The Guru Granth Sahib's teachings are centered around the worship of the one, formless God, the importance of Guru's grace, the equality of all human beings, and the rejection of caste and religious distinctions. Its hymns or Shabads emphasize the values of love, compassion, truth, devotion, and the remembrance of God (Naam Japna). For Sikhs, the Guru Granth Sahib is a guide to living a life that is spiritually fulfilling and morally upright.",
+  },
+  {
+    subheading: "Reverence and Protocol",
+    text: "In Sikh Gurdwaras (places of worship), the Guru Granth Sahib is treated with the utmost respect and honor. It is placed on a throne (Takht or Palki) under a canopy, and Sikhs bow before it as a sign of their submission to its teachings. The daily worship service involves the singing of its hymns, and the scripture is ceremonially opened in the morning and closed in the evening, followed by the Ardas (prayer).",
+  },
+  {
+    subheading: "Eternal Guru",
+    text: "Guru Gobind Singh, the tenth and last human Sikh Guru, declared the Guru Granth Sahib as the final and eternal Guru of the Sikhs before his passing in 1708. This declaration meant that Sikhs were to regard the Guru Granth Sahib as their spiritual leader, turning to it for guidance, solace, and inspiration. This marked a significant shift in religious authority from a person to a text, emphasizing the Sikh commitment to the Word as the embodiment of the divine.",
+  },
+  {
+    subheading: "A Living Scripture",
+    text: "The Guru Granth Sahib is not just a book to be read, but a living presence to be engaged with. Sikhs consider it a living Guru, and its teachings are applied to all aspects of life, guiding the community through its wisdom. Its universal messages of devotion to God, social justice, and human unity continue to inspire and guide not only Sikhs but people of all faiths around the world.",
+  },
+];
+
 function GranthSahib() {
   return (
     <PageLayout heading="the guru granth sahib">
-      <Section
-        text={
-          "The Guru Granth Sahib is the central religious scripture of Sikhism, revered not just as a sacred text but as the eternal Guru and the final authority in Sikh life and spirituality. Compiled and completed in 1604 by the fifth Sikh Guru, Guru Arjan Dev Ji, it embodies the teachings, wisdom, and divine poetry of the Sikh Gurus, along with saints and scholars from various religious backgrounds, reflecting Sikhism's inclusive ethos."
-        }
-      />
-
-      <ContentWithSubheading
-        subheading={"Composition and Contributors"}
-        text={
-          "The Guru Granth Sahib is a voluminous text composed in the Gurmukhi script and contains 1430 Angs (pages). It includes the compositions of the Sikh Gurus, from Guru Nanak Dev Ji to Guru Tegh Bahadur, and enlightened souls like Bhagat Kabir, Sheikh Farid, Bhagat Ravidas, and many others, making it a unique scripture that transcends religious boundaries. This diversity underscores the Sikh belief in the universality of God and the unity of all humanity."
-        }
-      />
-
-      <ContentWithSubheading
-        subheading={"teachings and significance"}
-        text={
-          "The Guru Granth Sahib's teachings are centered around the worship of the one, formless God, the importance of Guru's grace, the equality of all human beings, and the rejection of caste and religious distinctions. Its hymns or Shabads emphasize the values of love, compassion, truth, devotion, and the remembrance of God (Naam Japna). For Sikhs, the Guru Granth Sahib is a guide to living a life that is spiritually fulfilling and morally upright."
-        }
-      />
-
-      <ContentWithSubheading
-        subheading={"Reverence and Protocol"}
-        text={
-          "In Sikh Gurdwaras (places of worship), the Guru Granth Sahib is treated with the utmost respect and honor. It is placed on a throne (Takht or Palki) under a canopy, and Sikhs bow before it as a sign of their submission to its teachings. The daily worship service involves the singing of its hymns, and the scripture is ceremonially opened in the morning and closed in the evening, followed by the Ardas (prayer)."
-        }
-      />
-
-      <ContentWithSubheading
-        subheading={"Eternal Guru"}
-        text={
-          "Guru Gobind Singh, the tenth and last human Sikh Guru, declared the Guru Granth Sahib as the final and eternal Guru of the Sikhs before his passing in 1708. This declaration meant that Sikhs were to regard the Guru Granth Sahib as their spiritual leader, turning to it for guidance, solace, and inspiration. This marked a significant shift in religious authority from a person to a text, emphasizing the Sikh commitment to the Word as the embodiment of the divine."
-        }
-      />
-
-      <ContentWithSubheading
-        subheading={"A Living Scripture"}
-        text={
-          "The Guru Granth Sahib is not just a book to be read, but a living presence to be engaged with. Sikhs consider it a living Guru, and its teachings are applied to all aspects of life, guiding the community through its wisdom. Its universal messages of devotion to God, social justice, and human unity continue to inspire and guide not only Sikhs but people of all faiths around the world."
-        }
-      />
+      {granthSahibData.map((item) =>
+        // check if the item has a subheading
+        item.subheading ? (
+          <ContentWithSubheading key={item.subheading} {...item} />
+        ) : (
+          <Section key={item.text} text={item.text} />
+        )
+      )}
     </PageLayout>
   );
 }
--- src/sikhism/Turban.jsx
+++ src/sikhism/Turban.jsx
@@ -1,45 +1,64 @@
 import Section from "../components/Section";
 import PageLayout from "../components/PageLayout";
-import ContentwithSubheading from "../components/ContentWithSubheading";
+import ContentWithSubheading from "../components/ContentWithSubheading";
+// import HorizontalImagesForScroller, {
+//   HorizontalImageScroller,
+// } from "../components/HorizontalImageScroller";
+
+// import test from "../assets/five-k's/kesh.jpeg";
+
+// the text data
+const turbanData = [
+  {
+    text: "The turban, known as Dastar in Punjabi, holds profound significance in Sikh culture and religion. It is not merely a piece of cloth but a symbol of faith, honor, and identity for Sikhs. The turban represents a commitment to the values taught by the Sikh Gurus, including equality, sovereignty, and dedication to God.",
+  },
+  {
+    subheading: "Historical Significance",
+    text: "The practice of wearing a turban was mandated by Guru Gobind Singh Ji, the tenth Sikh Guru, in 1699 when he formed the Khalsa, a community of initiated Sikhs. Guru Gobind Singh Ji instructed his followers to wear the turban as a mark of dignity and as a way to maintain the unique identity of Sikhs. The turban was also intended to eliminate social hierarchies, as in the past, the right to wear a turban was reserved for the elite classes in many cultures.",
+  },
+  {
+    subheading: "Symbolism and Meaning",
+    text: "The turban symbolizes discipline, integrity, humility, and the willingness to protect the rights and honor of all people. It serves as a reminder to live according to the teachings of the Sikh Gurus and to uphold the principles of Sikhism, including the commitment to justice and service to humanity. Moreover, it signifies the Sikh's sovereignty and independence, echoing the community's history of resistance against oppression.",
+  },
+  {
+    subheading: "Varieties and Styles",
+    text: "Sikh turbans come in various styles, lengths, and colors, reflecting personal choice, regional influences, and occasions. The tying style can vary from one individual to another, with variations such as the Dumalla, Nok, and Patiala Shahi, among others. The color of the turban might hold personal significance or be chosen for aesthetic reasons, without any specific religious prescription.",
+  },
+  {
+    subheading: "The Turban in Daily Life",
+    text: "For Sikhs, wearing a turban is a daily practice that begins at a young age. It is an integral part of the Sikh identity, visibly distinguishing Sikhs in a crowd and symbolizing their commitment to their faith and values. The act of tying the turban is considered a meditative and respectful ritual, reflecting the wearer's spiritual and temporal dedication.",
+  },
+  {
+    subheading: "Beyond Fashion: A Commitment to Faith",
+    text: "While the turban is sometimes perceived merely as a cultural or fashion statement, for Sikhs, it is a profound expression of faith. The turban is a badge of responsibility and a crown of honor, signifying the wearer's dedication to living a life of courage, humility, and service to others. It is a constant reminder of the Sikh's duty to stand for righteousness and to defend the oppressed, embodying the spirit of the Khalsa.",
+  },
+];
 
 function Turban() {
   return (
     <PageLayout heading="The Turban (Dastar) in Sikhism">
-      <Section text="The turban, known as Dastar in Punjabi, holds profound significance in Sikh culture and religion. It is not merely a piece of cloth but a symbol of faith, honor, and identity for Sikhs. The turban represents a commitment to the values taught by the Sikh Gurus, including equality, sovereignty, and dedication to God." />
-      <ContentwithSubheading
-        subheading={"Historical Significance"}
-        text={
-          "The practice of wearing a turban was mandated by Guru Gobind Singh Ji, the tenth Sikh Guru, in 1699 when he formed the Khalsa, a community of initiated Sikhs. Guru Gobind Singh Ji instructed his followers to wear the turban as a mark of dignity and as a way to maintain the unique identity of Sikhs. The turban was also intended to eliminate social hierarchies, as in the past, the right to wear a turban was reserved for the elite classes in many cultures."
-        }
-      />
+      {turbanData.map((item) =>
+        // check if there is a subheading (only the first line won't have a subheading)
+        item.subheading ? (
+          <ContentWithSubheading key={item.subheading} {...item} />
+        ) : (
+          <Section key={item.text} text={item.text} />
+        )
+      )}
 
-      <ContentwithSubheading
-        subheading={"Symbolism and Meaning"}
-        text={
-          "The turban symbolizes discipline, integrity, humility, and the willingness to protect the rights and honor of all people. It serves as a reminder to live according to the teachings of the Sikh Gurus and to uphold the principles of Sikhism, including the commitment to justice and service to humanity. Moreover, it signifies the Sikh's sovereignty and independence, echoing the community's history of resistance against oppression."
-        }
-      />
+      {/* <HorizontalImageScroller>
+        <HorizontalImagesForScroller
+          imageSrc={test}
+          alt={"sikh man wearing a blue turban"}
+          // name={"turban"}
+        />
 
-      <ContentwithSubheading
-        subheading={"Varieties and Styles"}
-        text={
-          "Sikh turbans come in various styles, lengths, and colors, reflecting personal choice, regional influences, and occasions. The tying style can vary from one individual to another, with variations such as the Dumalla, Nok, and Patiala Shahi, among others. The color of the turban might hold personal significance or be chosen for aesthetic reasons, without any specific religious prescription."
-        }
-      />
-
-      <ContentwithSubheading
-        subheading={"The Turban in Daily Life"}
-        text={
-          "For Sikhs, wearing a turban is a daily practice that begins at a young age. It is an integral part of the Sikh identity, visibly distinguishing Sikhs in a crowd and symbolizing their commitment to their faith and values. The act of tying the turban is considered a meditative and respectful ritual, reflecting the wearer's spiritual and temporal dedication."
-        }
-      />
-
-      <ContentwithSubheading
-        subheading={"Beyond Fashion: A Commitment to Faith"}
-        text={
-          "While the turban is sometimes perceived merely as a cultural or fashion statement, for Sikhs, it is a profound expression of faith. The turban is a badge of responsibility and a crown of honor, signifying the wearer's dedication to living a life of courage, humility, and service to others. It is a constant reminder of the Sikh's duty to stand for righteousness and to defend the oppressed, embodying the spirit of the Khalsa."
-        }
-      />
+        <HorizontalImagesForScroller
+          imageSrc={test}
+          alt={"sikh man wearing a blue turban"}
+          // name={"turban"}
+        /> */}
+      {/* </HorizontalImageScroller> */}
     </PageLayout>
   );
 }
